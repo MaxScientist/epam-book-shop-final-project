bb1c64a0e8e6559b3abf716c57bd8de47b4867db
MaxScientist <maksatgalymzhan@gmail.com>
2022-03-10 11:17:55 +0600

    Update(Replacing params to class)

--- src/main/java/com/epam/bookshop/database/connection/ConnectionPool.java
+++ src/main/java/com/epam/bookshop/database/connection/ConnectionPool.java
@@ -3,10 +3,8 @@ package com.epam.bookshop.database.connection;
 import org.apache.log4j.Logger;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ResourceBundle;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -14,42 +12,40 @@ import java.util.concurrent.BlockingQueue;
 public final class ConnectionPool {
 
     private final Logger LOGGER = Logger.getLogger(this.getClass().getName());
-    private String url;
-    private String user;
-    private String password;
-    private String driverName;
-    private final ResourceBundle properties = ResourceBundle.getBundle("ConnectionPool");
-    private final int POOL_SIZE = Integer.parseInt(properties.getString("db.poolsize"));
+    private final int POOL_SIZE = DataBaseSupplier.POOL_SIZE;
     private static volatile ConnectionPool instance;
     private final BlockingQueue<Connection> connectionQueue = new ArrayBlockingQueue<>(POOL_SIZE);
 
     private ConnectionPool() {
-        init();
-    }
-
-    private void init() {
-        setDataForConnection();
         loadDrivers();
         initPoolData();
-    }
 
-    private void setDataForConnection() {
-        this.driverName = properties.getString("db.driver");
-        this.url = properties.getString("db.url");
-        this.user = properties.getString("db.user");
-        this.password = properties.getString("db.password");
     }
 
     private void loadDrivers() {
         try {
-            Driver driver = (Driver) Class.forName(driverName).newInstance();
-        } catch (InstantiationException | ClassNotFoundException e) {
-            LOGGER.error(e);
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            LOGGER.warn(e);
+            Class.forName(DataBaseSupplier.DB_DRIVER);
+        } catch (ClassCastException | ClassNotFoundException e) {
+           LOGGER.error(e);
+           e.printStackTrace();
         }
     }
+
+    private void initPoolData() {
+        Connection connection;
+
+        while (connectionQueue.size() < POOL_SIZE) {
+            try {
+                connection = DriverManager.getConnection(DataBaseSupplier.DB_URL, DataBaseSupplier.DB_USER, DataBaseSupplier.DB_PASSWORD);
+
+                connectionQueue.put(connection);
+            } catch (InterruptedException | SQLException e) {
+                LOGGER.warn(e);
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     public static ConnectionPool getInstance() {
         ConnectionPool localInstance = instance;
@@ -62,20 +58,6 @@ public final class ConnectionPool {
             }
         }
         return localInstance;
-    }
-
-    private void initPoolData() {
-        Connection connection;
-
-        while (connectionQueue.size() < POOL_SIZE) {
-            try {
-                connection = DriverManager.getConnection(url, user, password);
-                connectionQueue.put(connection);
-            } catch (InterruptedException | SQLException e) {
-                LOGGER.warn(e);
-                e.printStackTrace();
-            }
-        }
     }
 
     public synchronized Connection takeConnection() {
---
+++ src/main/java/com/epam/bookshop/database/connection/DataBaseSupplier.java
@@ -0,0 +1,26 @@
+package com.epam.bookshop.database.connection;
+
+import java.util.ResourceBundle;
+
+public final class DataBaseSupplier {
+    public static final String DB_DRIVER;
+    public static final String DB_URL;
+    public static final String DB_USER;
+    public static final String DB_PASSWORD;
+    public static final int POOL_SIZE;
+    private static final ResourceBundle properties = ResourceBundle.getBundle("ConnectionPool");
+
+    private DataBaseSupplier(){
+    }
+
+    static {
+        DB_DRIVER = properties.getString("db.driver");
+        DB_URL = properties.getString("db.url");
+        DB_USER = properties.getString("db.user");
+        DB_PASSWORD = properties.getString("db.password");
+        POOL_SIZE = Integer.parseInt(properties.getString("db.poolsize"));
+    }
+
+
+
+}
