f6a9dc44857fcd99f07ebcdebbc4ac7b0cc9dcb2
MaxScientist <maksatgalymzhan@gmail.com>
2022-03-24 18:07:38 +0600

    Update

--- src/main/java/com/epam/bookshop/action/implementation/AboutUsAction.java
+++ src/main/java/com/epam/bookshop/action/implementation/AboutUsAction.java
@@ -7,13 +7,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 
 import static com.epam.bookshop.constants.PageNameConstants.ABOUT_US_PAGE;
 
 public class AboutUsAction implements Action {
     @Override
-    public void execute(HttpServletRequest req, HttpServletResponse resp) throws SQLException, ServletException, IOException {
+    public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher dispatcher = req.getRequestDispatcher(ABOUT_US_PAGE);
         dispatcher.forward(req, resp);
     }
